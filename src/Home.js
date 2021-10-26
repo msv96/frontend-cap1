692d97c57d9b81ddeffeef793b0b86ff7eb2b157
msv96 <msrini1100@gmail.com>
2021-10-26 20:49:28 +0530

    10% done

--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,32 @@ const Home = () => {
 		<div className="home">
 			<h2>Office Planning Tool</h2>
 			<div>
-				Hello, welcome to our platform where beginners can easily
-				<br />
-				learn to use and helps everyone to be productive.
+				Hello, welcome to our platform where beginners can easily learn
+				to use.
+			</div>
+			<div>
+				<div className="forms">
+					<h4>Signin to Explore</h4>
+                    <form className="form_flex">
+                        <label htmlFor="mailid">E-mail ID</label>
+                        <input type="email" name="email" id="mailid" className="input" />
+                        <label htmlFor="pwd">Password</label>
+                        <input type="password" name="password" id="pwd" className="input" />
+                        <input type="submit" value="Login" className="btn" />
+                    </form>
+				</div>
+                <div className="forms">
+                    <h4>Don't have an account, Register</h4>
+                    <form className="form_flex">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" className="input" />
+                        <label htmlFor="mail">E-mail ID</label>
+                        <input type="email" name="email" id="mail" className="input" />
+                        <label htmlFor="pwd1">Password</label>
+                        <input type="password" name="password" id="pwd1" className="input" />
+                        <input type="submit" value="Register" className="btn" />
+                    </form>
+                </div>
 			</div>
 		</div>
 	);
